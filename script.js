28bbb04eb00552a5661ee06acb9dca973145ea37
M1guelL1ma <miglimauel@gmail.com>
2025-06-10 17:24:18 -0300

    lista de tarefas funcional

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const tarefa = document.getElementById('tarefa');
 const taskList = document.getElementById('taskList');
 const titulo = document.getElementById('titulo');
 
-let mome = prompt("Qual é o seu nome?")
+let nome = prompt("Qual é o seu nome?")
 if (nome==""){
-titulo.innerHTML = `Lista de tarefa: ${nome}`;
+titulo.innerHTML = `Lista de tarefa generico`;
 }
 else{
     titulo.innerHTML = `Lista de tarefa: ${nome}`;
@@ -13,5 +13,30 @@ else{
 btnAdd.addEventListener("click",criarTarefa);
 
 function criarTarefa(){
+    const listItem = document.createElement('li')
+    listItem.textContent = tarefa.value;
+    taskList.appendChild(listItem)
+
+    const removeButton = document.createElement('button');
+    removeButton.id = "remove";
+    removeButton.textContent = "x"
     
+
+    removeButton.addEventListener("click", function (){
+        taskList.removeChild(listItem);
+    })
+
+    const concluirButton = document.createElement('button');
+    concluirButton.id = "conclui";
+    concluirButton.textContent = "✓";
+
+    let buttonsItem = document.createElement('div');
+    buttonsItem.classList.toggle('buttonsItem')
+    listItem.appendChild(buttonsItem);
+    buttonsItem.appendChild(concluirButton);
+    buttonsItem.appendChild(removeButton);
+
+    concluirButton.addEventListener("click", function(){
+        listItem.classList.toggle('completed');
+    })
 }

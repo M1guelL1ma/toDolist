909fc8f7895237366d87344cdc21f214dd30a80e
M1guelL1ma <miglimauel@gmail.com>
2025-06-12 09:14:45 -0300

    envio com enter e focus

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ const titulo = document.getElementById('titulo');
 
 let nome = prompt("Qual é o seu nome?")
 if (nome==""){
-titulo.innerHTML = `Lista de tarefa generico`;
+titulo.innerHTML = `Lista de tarefas do generico`;
 }
 else{
-    titulo.innerHTML = `Lista de tarefa: ${nome}`;
+    titulo.innerHTML = `Lista de tarefas do(a): ${nome}`;
+    tarefa.focus();
 }
 btnAdd.addEventListener("click",criarTarefa);
 
+tarefa.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') criarTarefa();
+});
+
 function criarTarefa(){
+    if (tarefa.value==""){
+        alert("Digite o nome da sua tarefa")
+    }
+    else{
     const listItem = document.createElement('li')
     listItem.textContent = tarefa.value;
     taskList.appendChild(listItem)
@@ -39,4 +48,8 @@ function criarTarefa(){
     concluirButton.addEventListener("click", function(){
         listItem.classList.toggle('completed');
     })
+
+    tarefa.value = '';
+    tarefa.focus();
+        }
 }
